636575b6f84c448a7a492712f0489b47fde792db
KarimAmador <karimalejandroAC@gmail.com>
2024-07-23 08:49:11 -0600

    chore: delete console logs

--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,6 @@ module.exports = function (app, createModel) {
   
     .get(async function (req, res){
       let project = req.params.project;
-      console.log(project);
 
       const Project = createModel(project);
       let issues = await Project.find().select({__v: 0}).exec();
@@ -16,7 +15,6 @@ module.exports = function (app, createModel) {
     
     .post(async function (req, res){
       let project = req.params.project;
-      console.log(project); 
       console.log(req.body);
 
       const Project = createModel(project);
@@ -28,13 +26,11 @@ module.exports = function (app, createModel) {
     
     .put(function (req, res){
       let project = req.params.project;
-      console.log(project);
       
     })
     
     .delete(function (req, res){
       let project = req.params.project;
-      console.log(project);
       
     });
     
